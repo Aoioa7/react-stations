25c88669a6b197e34f6ddf7f8541a9deb6105044
aoi <149755797+Aoioa7@users.noreply.github.com>
2024-01-06 18:26:29 +0900

    ok

--- src/BreedsSelect.jsx
+++ src/BreedsSelect.jsx
@@ -5,7 +5,7 @@ var dogList=[]
 
 export const BreedsSelect=(props,dogList)=>{
   for(let i=0;i<props.breeds.length;i++){
-    dogList.push(<option value={i}>{props.breeds[i]}</option>)
+    dogList.push('<option value={i}>{props.breeds[i]}</option>')
   }
   return(
     <select name="whichBreed">
