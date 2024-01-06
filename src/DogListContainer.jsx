d404724b0d6c2a70e1b6014b1a23915cced4c383
aoi <149755797+Aoioa7@users.noreply.github.com>
2024-01-06 17:29:49 +0900

    ok

--- src/DogListContainer.jsx
+++ src/DogListContainer.jsx
@@ -1,17 +1,18 @@
 // @ts-check
+import React from 'react'
 import {useState} from 'react'
 import {useEffect} from 'react'
 
 export const DogListContainer = () => {
 
-  var [breeds,setBreeds] =useState([])
-  var getBreeds=()=>{fetch("https://dog.ceo/api/breeds/list/all").then(response => response.json()).then(data => setBreeds(data.message))}
+  const [breeds,setbreeds] =useState([])
+  var getBreeds=()=>{fetch("https://dog.ceo/api/breeds/list/all").then(response => response.json()).then(data => setbreeds(data.message))}
 
   useEffect(()=>{getBreeds},[])
 
-  return (
+  /*return (
     <footer></footer>
-  )
+  )*/
 }
 
 export default DogListContainer
