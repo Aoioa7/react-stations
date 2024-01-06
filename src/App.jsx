dec5666d16e0ed38b4a72bd1b1e4798b10225f16
aoi <149755797+Aoioa7@users.noreply.github.com>
2024-01-06 13:08:31 +0900

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 // DO NOT DELETE
 
 //import { s } from 'vitest/dist/reporters-cb94c88b'
+
 import './App.css'
 import { useState } from 'react'
 
@@ -8,9 +9,9 @@ import { useState } from 'react'
  @type {() => JSX.Element}
 */
 export const App = () => {
-  var [dogUrl,setDogUrl] = useState("https://images.dog.ceo/breeds/pitbull/20190801_154956.jpg");
+  var [dogUrl,setDogUrl] = useState("https://images.dog.ceo/breeds/pitbull/20190801_154956.jpg",{method:'GET'});
   var next;
-  fetch("https://dog.ceo/api/breeds/image/random").then(response => response.text()).then(data => {next=data["message"]});
+  fetch("https://dog.ceo/api/breeds/image/random").then(response => response.json()).then(data => {next=data["message"]});
   var renew=()=>{setDogUrl(dogUrl=next)};
   return (
     <body>
