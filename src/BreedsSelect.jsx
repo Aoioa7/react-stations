577d4247cdbb13807172f712b13108220a75e559
aoi <149755797+Aoioa7@users.noreply.github.com>
2024-01-06 18:12:03 +0900

    ok

--- src/BreedsSelect.jsx
+++ src/BreedsSelect.jsx
@@ -1,7 +1,18 @@
 // @ts-check
 
-export const BreedsSelect = () => {
-  return <></>
+// @ts-ignore
+dogList=[]
+export const BreedsSelect = (props,dogList) => {
+  for(let i=0;i<props.breeds.length;i++){
+    dogList.push(<option value={i}>{props.breeds[i]}</option>)
+  }
+  return (
+    <div>
+      <select name= "whichBreed">
+        {dogList}
+      <select/>
+    </div>
+  )
 }
 
 export default BreedsSelect
--- src/DogListContainer.jsx
+++ src/DogListContainer.jsx
@@ -2,18 +2,22 @@
 import React from 'react'
 import {useState} from 'react'
 import {useEffect} from 'react'
+import {BreedsSelect} from './BreedsSelect'
 
 export const DogListContainer = () => {
 
   const [breeds,setBreeds] =useState([])
   useEffect(()=>{
     fetch("https://dog.ceo/api/breeds/list/all").then(response => response.json())
+    // @ts-ignore
     .then(data => setBreeds(data.message)).then(console.log(breeds))
 
   },[])
 
   return (
-    <footer></footer>
+    <footer>
+      <BreedsSelect breeds={breeds} />
+    </footer>
   )
 }
 
