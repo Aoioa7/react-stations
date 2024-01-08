a64cf255050da5fb4a29e769d7c228c20d8513fb
aoi <149755797+Aoioa7@users.noreply.github.com>
2024-01-08 13:04:12 +0900

    ok

--- src/BreedsSelect.jsx
+++ src/BreedsSelect.jsx
@@ -12,21 +12,43 @@ export const BreedsSelect=(props)=>{
        return <option value={breed}>{breed}</option>
       }
     )
-  
-
 
   var[selected,setSelected]=useState('');
   // @ts-ignore
   const handleChange=e=>{
     setSelected(e.target.value)
   } ;
+  //
+  var[looking,setLooking]=useState([]);
+  const look=()=>{
+    var x=selected;
+    fetch(`https://dog.ceo/api/breed/${x}/images/random/10`).then(response => response.json())
+    // @ts-ignore
+    .then(data => {setLooking(data.message)})
+    .then(
+      ()=>{
+        looking.forEach((y)=>console.log(`<img src="${y}">`))
+      }
+    )
+  }
+
+  const images= 
+    looking.map(
+    (z)=>{
+      return <img src={z} />
+    }
+  )
+  
+  //
 
   return (
     <div>
       <select value={selected} onChange={handleChange}>
         <option value="">犬種を選んでね</option>
-        {selectOptions};
+        {selectOptions}
       </select>
+      <button onClick={look}>表示</button>
+      {images}
     </div>
   )
 }
